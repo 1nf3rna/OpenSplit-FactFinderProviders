c1a4f7d3623b615046f27835ebd3a510d076d197
Douglas Kirby <douglas.r.kirby@gmail.com>
2026-05-25 16:36:33 -0400

    added addrs and state display info

--- BattleToads/NES/All Stages Co-op/factbuilder.lua
+++ BattleToads/NES/All Stages Co-op/factbuilder.lua
@@ -1,47 +1,82 @@
+addrs = {
+    start = 0x0,
+    RagnaroksCanyon = 0x1,
+    WookieHole = 0x2,
+    TurboTunnel = 0x3,
+    ArcticCaverns = 0x4,
+    SurfCity = 0x5,
+    KarnathsLair = 0x6,
+    VolkmiresInferno = 0x7,
+    IntruderExcluder = 0x8,
+    TerraTubes = 0x9,
+    RatRace = 0xA,
+    ClingerWinger = 0xB,
+    TheRevolution = 0xC,
+    QueenRoom = 0xD,
+    Cutscene = 0xFF,
+    QueenDead = 0x5,
+    QueenAlive = 0x0,
+}
+
+state = {
+    started = false,
+    currentLevel = 0,
+}
+
 function onTick()
-    checkStart()
-    checkReset()
+    if not state.started then
+        checkStart()
+    end
+    if state.started then
+        checkReset()
+    end
     checkStage()
 end
 
 function checkStage()
-    if level == 0xD
-    and qdead == 0x5
-    and qdead_last == 0x0 then
+    state.currentLevel = level
+    if level == addrs.QueenRoom
+    and qdead == addrs.QueenDead
+    and qdead_last == addrs.QueenAlive then
         split()
+        state.started = false
         return
     end
-    if level_last == 0xFF
-    and (level == 0x2
-    or level == 0x3
-    or level == 0x4
-    or level == 0x5
-    or level == 0x6
-    or level == 0x7
-    or level == 0x8
-    or level == 0x9
-    or level == 0xA
-    or level == 0xB
-    or level == 0xC
-    or level == 0xD) then
+    if level_last == addrs.Cutscene
+    and (level == addrs.WookieHole
+    or level == addrs.TurboTunnel
+    or level == addrs.ArcticCaverns
+    or level == addrs.SurfCity
+    or level == addrs.KarnathsLair
+    or level == addrs.VolkmiresInferno
+    or level == addrs.IntruderExcluder
+    or level == addrs.TerraTubes
+    or level == addrs.RatRace
+    or level == addrs.ClingerWinger
+    or level == addrs.TheRevolution
+    or level == addrs.QueenRoom) then
         split()
+
         return
     end
 end
 
-
 function checkStart()
-    if level == 0x1
-    and level_last == 0x0
+    state.currentLevel = level
+    if level == addrs.RagnaroksCanyon
+    and level_last == addrs.start
     and player2 > 0 then
         split()
+        state.started = true
     end
 end
 
 function checkReset()
-    if level == 0
-    and level_last ~= 0 then
+    state.currentLevel = level
+    if level == addrs.start
+    and level_last ~= addrs.start then
         reset()
+        state.started = false
         return
     end
 end
