f9f9464c3542c4e663c2f62ff778bc1c46f3c262
Douglas Kirby <douglas.r.kirby@gmail.com>
2026-05-25 17:25:36 -0400

    updated for some addr variables

--- Soldiers of Fortune/SNES/Single Segment/factbuilder.lua
+++ Soldiers of Fortune/SNES/Single Segment/factbuilder.lua
@@ -1,5 +1,44 @@
+addrs = {
+    TheBeginning = 0x0,
+    MudRivers = 0x1,
+    Rings = 0x2,
+    TheRockies = 0x3,
+    Maze = 0x4,
+    Traps = 0x5,
+    Steam = 0x6,
+    Quarters = 0x7,
+    ThePits = 0x8,
+    Confusion = 0x9,
+    Reverse = 0xA,
+    WayOut = 0xB,
+    Sewers = 0xC,
+    PumpRoom = 0xD,
+    HallOfMachines = 0xE,
+    TimeForChaos = 0xF,
+    Start = 0xFF,
+    Forest = 0x1,
+    Workshops = 0x2,
+    Mansion = 0x3,
+    Cellars = 0x4,
+    Chaos = 0x5,
+    Complete = 0x40,
+    Score = 0x80,
+    DuringShop = 0x80,
+    BeforeShop = 0xA,
+}
+
 state = {
-    started = false
+    started = false,
+    level = 0x0,
+    world = 0x0,
+    player1input = 0x0,
+    player2input = 0x0,
+    BossHP = 0x0,
+    exitCount = 0x0,
+    levelComplete = 0x0,
+    scoreScreen = 0x0,
+    shopPlayerID = 0x0,
+    shopScreen = 0x0,
 }
 
 function onTick()
@@ -13,11 +52,11 @@ function onTick()
 end
 
 function checkStage()
-    if level == 0xF
-    and world == 0x5 then
+    if level == addrs.TimeForChaos
+    and world == addrs.Chaos then
         -- check final boss state
         if BossHP == 0x0
-        and BossHP_last > 0 then
+        and BossHP_last > 0x0 then
             if shopPlayerID == 0x33 then
                 split()
                 return
@@ -29,7 +68,7 @@ function checkStage()
             end
         end
         --check 4-4 "finish" state
-        if exitCount > 0
+        if exitCount > 0x0
         and (player1input_last == 0x0
         or player2input_last == 0x0)
         and shopScreen_last == 0x78
@@ -41,12 +80,12 @@ function checkStage()
     -- check level finish state
     if (player1input > 0x0
     or player2input > 0x0)
-    and exitCount > 0
+    and exitCount > 0x0
     and scoreScreen_last == 0x0
-    and scoreScreen == 0x80 then
+    and scoreScreen == addrs.Score then
         -- check 5th shop
         if shopScreen_last == 0x0
-        and shopScreen == 0x0A
+        and shopScreen == addrs.BeforeShop
         and shopPlayerID_last == 0x1D
         and shopPlayerID == 0x1D
         and levelComplete == 0x0 then
@@ -54,8 +93,8 @@ function checkStage()
             return
         end
         -- check shop state
-        if shopScreen_last == 0x0A
-        and shopScreen == 0x80
+        if shopScreen_last == addrs.BeforeShop
+        and shopScreen == addrs.DuringShop
         and ((shopPlayerID_last == 0x23
         and shopPlayerID == 0x24)
         or (shopPlayerID_last == 0x17
@@ -73,9 +112,9 @@ function checkStage()
             return
         end
         -- check main levels state
-        if levelComplete == 0x40
+        if levelComplete == addrs.Complete
         and shopScreen_last == 0x0
-        and shopScreen == 0x80 then
+        and shopScreen == addrs.DuringShop then
             split()
             return
         end
@@ -84,11 +123,11 @@ end
 
 
 function checkStart()
-    if world == 0xFF
+    if world == addrs.Start
     and exitCount == 0x0
     and levelComplete == 0x0 then
         if shopScreen_last == 0x0
-        and shopScreen == 0x80 then
+        and shopScreen == addrs.DuringShop then
             split()
             state.started = true
             return
@@ -97,8 +136,8 @@ function checkStart()
 end
 
 function checkReset()
-    if level == 0
-    and world == 0 then
+    if level == addrs.TheBeginning
+    and world == 0x0 then
         reset()
         state.started = false
         return
